b7b618fb3f8a6ce7fae3551ca95037f2199d7f54
drawmoon <1340260725@qq.com>
2022-01-24 12:05:53 +0800

    处理导出为 word，<label> 标签无法渲染的问题

--- index.ts
+++ index.ts
@@ -81,81 +81,93 @@ const HTML_TEMPLATE = `
 </body>
 </html>`;
 
-const expectImageWidth = 595;
-const expectImageHeight = 842;
+const labelPattern = /<label[^>]*>/gm;
 
-async function resize(file: string): Promise<string> {
-    const svg = await loadImage(file);
+interface ConvertOptions {
+  expectWidth?: number | undefined;
+  expectHeight?: number | undefined;
+};
 
-    let width = svg.width;
-    let height = svg.height;
+async function resize(file: string, options?: ConvertOptions | undefined): Promise<string> {
+  const { expectWidth = 595, expectHeight = 842 } = options ?? {};
 
-    if (width > expectImageWidth) {
-        height = (height * expectImageWidth) / width;
-        width = expectImageWidth;
-    } else if (height > expectImageHeight) {
-        width = (width * expectImageHeight) / height;
-        height = expectImageHeight;
-    }
+  const svg = await loadImage(file);
 
-    console.log(`Redrawing image, Width: ${width}, Height: ${height}`);
+  let width = svg.width;
+  let height = svg.height;
 
-    const canvas = createCanvas(width, height);
-    const context = canvas.getContext('2d');
-    context.drawImage(svg, 0, 0, width, height);
-    return canvas.toDataURL('image/png');
+  if (width > expectWidth) {
+    height = (height * expectWidth) / width;
+    width = expectWidth;
+  } else if (height > expectHeight) {
+    width = (width * expectHeight) / height;
+    height = expectHeight;
+  }
+
+  console.log(`Redrawing image, Width: ${width}, Height: ${height}`);
+
+  const canvas = createCanvas(width, height);
+  const context = canvas.getContext('2d');
+  context.drawImage(svg, 0, 0, width, height);
+  return canvas.toDataURL('image/png');
 }
 
 async function convertImg(html: string): Promise<string> {
-    const dom = new JSDOM(html);
-    const doc = dom.window.document;
+  const dom = new JSDOM(html);
+  const doc = dom.window.document;
 
-    console.log('Converting image file to base64.');
+  console.log('Converting image file to base64.');
 
-    for (const img of doc.querySelectorAll('img')) {
-        const imgFile = join(basePath, img.src);
+  for (const img of doc.querySelectorAll('img')) {
+    const imgFile = join(basePath, img.src);
 
-        img.src = await resize(imgFile);
-    }
+    img.src = await resize(imgFile);
+  }
 
-    return doc.body.innerHTML;
+  return doc.body.innerHTML;
 }
 
 async function readHtml(): Promise<string> {
-    let htmlStr = readFileSync(join(basePath, 'index.html'), {
-        encoding: 'utf-8',
-    });
+  let htmlStr = readFileSync(join(basePath, 'index.html'), {
+      encoding: 'utf-8',
+  });
 
-    htmlStr = await convertImg(htmlStr);
+  // 处理 <label> 标签无法渲染的问题
+  htmlStr = htmlStr.replace(labelPattern, (str) => {
+    return str.replace('label', 'p');
+  });
 
-    return htmlStr;
+  // 将图片转换为 Base64
+  htmlStr = await convertImg(htmlStr);
+
+  return htmlStr;
 }
 
 async function toPdf(): Promise<Buffer> {
-    const htmlContent = await readHtml();
-    const html = HTML_TEMPLATE.replace('{htmlContent}', htmlContent);
+  const htmlContent = await readHtml();
+  const html = HTML_TEMPLATE.replace('{htmlContent}', htmlContent);
 
-    return await pdf.generatePdf(
-        { content: html },
-        { format: 'A4', margin: { top: 40, right: 20, bottom: 40, left: 20 } },
-    );
+  return await pdf.generatePdf(
+    { content: html },
+    { format: 'A4', margin: { top: 40, right: 20, bottom: 40, left: 20 } },
+  );
 }
 
 async function toWord(): Promise<Buffer> {
-    const htmlContent = await readHtml();
-    const html = HTML_TEMPLATE.replace('{htmlContent}', htmlContent);
+  const htmlContent = await readHtml();
+  const html = HTML_TEMPLATE.replace('{htmlContent}', htmlContent);
 
-    return await word(html, null, {
-        table: { row: { cantSplit: true } },
-    });
+  return await word(html, null, {
+    table: { row: { cantSplit: true } },
+  });
 }
 
 // 保存 docx 文件
 toWord().then((buff) => {
-    writeFileSync(join(outputPath, 'dist/document.docx'), buff);
+  writeFileSync(join(outputPath, 'dist/document.docx'), buff);
 });
 
 // 保存 pdf 文件
 toPdf().then((buff) => {
-    writeFileSync(join(outputPath, 'dist/document.pdf'), buff);
+  writeFileSync(join(outputPath, 'dist/document.pdf'), buff);
 });
